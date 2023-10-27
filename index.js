6beddf97b7223fa205d53e0e02ce140ce5493598
Vishnu Jangid <vishnu.j8503@gmail.com>
2023-10-27 17:19:19 +0530

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import 'dotenv/config'
+
 
 import postRoutes from './routes/posts.js';
 
@@ -14,10 +16,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://vishnuj8503:Vishnu@8503@cluster0.s91gm2x.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT|| 5000;
+console.log(process.env.CONNECTION_URL) 
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`${CONNECTION_URL}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
